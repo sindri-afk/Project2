cd4b44ebf165443c34a406b5c71cbd027aa308c4
Haukur B <haukurb23@ru.is>
2024-11-04 10:24:26 +0000

    Update event_consumer.py

--- event_consumer.py
+++ event_consumer.py
@@ -20,38 +20,3 @@ class EventConsumer:
         
         channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
         channel.start_consuming()
-
-    
-
-
-
-
-
-
-'''
-import pika
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        "localhost", credentials=pika.PlainCredentials("guest", "guest")
-    )
-)
-channel = connection.channel()
-
-channel.exchange_declare(exchange="hello-world", exchange_type="fanout")
-
-channel.queue_declare(queue="hello-consumer-1")
-
-channel.queue_bind(exchange="hello-world", queue="hello-consumer-1")
-
-
-def callback(ch, method, properties, body):
-    print(f" [x] Received from consumer 1 {body}")
-
-
-channel.basic_consume(
-    queue="hello-consumer-1", auto_ack=True, on_message_callback=callback
-)
-
-channel.start_consuming()
-'''
